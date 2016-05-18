06969e6deeb61fc67e059b7e33d3b39243d192f0
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2016-05-19 11:51:58 +1200

    subtract stream and readme checklist

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # conways-garden
+
+Todo
+
+- [ ] make a grid
+- [ ] create a gardener (who can move)
+- [ ] the gardener can plant a plant
+- [ ] the gardener can remove a plant
+- [ ] a plant remains dormant for a time
+- [ ] plants then follow conway rules
+- [ ] make a world pulse, each pulse iterates conway
+- [ ] pulse plays a sound per living plant
+- [ ] y access the key
+- [ ] x access the octave
+- [ ] distance from gardner inverse volume
+- [ ] plant selector (1, 2, 3, 4, 5)
+- [ ] 1/2 beat plant, 1/4, 1/8 etc 
--- index.js
+++ index.js
@@ -3,19 +3,30 @@ import {makeDOMDriver, div, button} from '@cycle/dom'
 import {Observable} from 'rx'
 
 function main({DOM}) {
-  const click$ = DOM
+  const add$ = DOM
     .select('.add')
     .events('click')
     .map((event) => 1);
 
-  const total$ = click$
+  const subtract$ = DOM
+    .select('.subtract')
+    .events('click')
+    .map((event) => -1);
+
+  const change$ = Observable.merge(
+    add$,
+    subtract$
+  )
+
+  const total$ = change$
     .startWith(0)
     .scan((total, change) => total + change)
 
   return {
     DOM: total$.map(total =>
       div([
-        button('.add', {style: {color: 'red'}}, 'Add'),
+        button('.add', {style: {color: 'green'}}, '+'),
+        button('.subtract', {style: {color: 'red'}}, '-'),
         div(`count: ${total}`)
       ])
     )
